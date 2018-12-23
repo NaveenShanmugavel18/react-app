57cb10d726079f477b581b8c7d48e32e9b1d803d
Naveen Shanmugavel <naveensv18@gmail.com>
2018-12-23 21:29:00 +0530

    updated app, inventory, addfish component

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+class AddFishForm extends React.Component {
+
+	createFish(e) {
+		e.preventDefault()
+
+		const fish = {
+			name: this.name.value ? this.name.value : '',
+			price: this.price.value ? this.price.value : '',
+			status: this.status.value ? this.status.value : '',
+			desc: this.description.value ? this.description.value : '',
+			image: this.image.value ? this.image.value : '',
+		}
+		this.props.addFish(fish)
+		this.fishForm.reset()
+	}
+
+	render() {
+		return (
+			<form ref={(input) => this.fishForm = input } action="" className="fish-edit" onSubmit={(e) => this.createFish(e)}>
+				<input type="text" placeholder="Fish Name" ref={(input) => {this.name = input}} />
+				<input type="text" placeholder="Fish Price" ref={(input) => {this.price = input}} />
+				<select ref={(input) => {this.status = input}}>
+					<option value="available">Fresh!</option>
+					<option value="unavailable">Sold Out!</option>
+				</select>
+				<textarea ref={(input) => {this.description = input}} placeholder="Fish Desc"></textarea>
+				<input ref={(input) => {this.image = input}} type="text" placeholder="Fish Image"/>
+				<button type="submit">+ Add Item</button>
+			</form>
+		)
+	}
+}
+
+export default AddFishForm
--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,63 @@ import React from 'react'
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
+import Fish from './Fish'
 
 class App extends React.Component {
+	constructor() {
+		super();
+
+		this.addFish = this.addFish.bind(this)
+		this.loadSamples = this.loadSamples.bind(this)
+		this.addToOrder = this.addToOrder.bind(this)
+
+		// initial state
+		this.state = {
+			fishes: {},
+			order: {}
+		};
+	}
+
+	addFish(fish) {
+		// Best practice to get a copy of fishes
+		const fishes = {...this.state.fishes}
+		let timestamp = Date.now()
+		// add fish
+		fishes[`fish-${timestamp}`] = fish
+		// update state
+		this.setState({ fishes: fishes })
+	}
+
+	loadSamples() {
+		this.setState({
+			fishes: sampleFishes
+		})
+	}
+
+	addToOrder(key) {
+		const order = {...this.state.order}
+		order[key] = order[key] + 1 || 1;
+		this.setState({ 
+			order: order
+		})
+	}
+
 	render() {
 		return(
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market" />
+					<ul className="list-of-fishes">
+						{
+							Object
+								.keys(this.state.fishes)
+								.map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} /> )
+						}
+					</ul>
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		)
 	}
